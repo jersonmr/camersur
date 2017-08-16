7017fc6efc2c2d41be9d168034ff87c7ebfb4bec
Jerson Moreno <venesoftware@gmail.com>
2017-08-16 15:28:48 -0400

    Agregados iconos nuevos de servicios y vistas de suscripcion y empresas

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+//- Detectando numero de items dentro del carousel
+var numItems = $(".owl-carousel").find('.owl-img').length;
+
+//Galería de imágenes con owl-carousel
+$('.owl-carousel').owlCarousel({
+  autoplay: true,
+  margin: 0,
+  nav: true,
+  autoWidth: false,
+  navText: ['<i class="fa fa-chevron-left" title="Anterior"></i>', '<i class="fa  fa-chevron-right" title="Siguiente"></i>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 2,
+      margin: 20,
+      loop: (numItems > 2) ? true : false,
+    },
+    992: {
+      items: 3,
+      margin: 15,
+      loop: (numItems > 3) ? true : false,
+    },
+  }
+});
